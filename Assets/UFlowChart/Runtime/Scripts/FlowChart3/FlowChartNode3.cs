17eeda955a0b47a4079f9862a27ef5c846c355e1
ZhangJiaqing <45001907+ZSaltedFish@users.noreply.github.com>
2023-11-27 02:34:20 +0900

    创建新Branch

---
+++ Assets/UFlowChart/Runtime/Scripts/FlowChart3/FlowChartNode3.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+namespace ZKnight.UFlowChart.Runtime
+{
+    public class FlowChartNode3 : MonoBehaviour
+    {
+        public int ChartID;
+    }
+}
---
+++ Assets/UFlowChart/Runtime/Scripts/FlowChart3/RuntimeType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ZKnight.UFlowChart.Runtime
+{
+    [Serializable]
+    public class RuntimeType<T>
+    {
+        public int IntValue;
+        public string StringValue;
+        public bool BoolValue;
+        public float FloatValue;
+        public UnityEngine.Object UnityObjValue;
+
+        public bool IsDynamicValue;
+        public string RuntimeKey;
+
+
+    }
+}
